1d9cd1b68d5368bbb40525cd2f28e40987fde19a
Mehdi Hatami <mhatami@sequencecorp.com>
2020-05-10 22:06:34 -0400

    Updates

---
+++ precache-manifest.378ec0a153eeb515292c6fa9eed01fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2f4e4e2bc6cddf8112947eb5e4f5db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d9423a56239b08f5c84a",
+    "url": "/monsters-rolodex/static/css/main.1573f0d8.chunk.css"
+  },
+  {
+    "revision": "639c49fbd6946a082aa3",
+    "url": "/monsters-rolodex/static/js/2.eb0c56c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.eb0c56c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9423a56239b08f5c84a",
+    "url": "/monsters-rolodex/static/js/main.f2ffa5f1.chunk.js"
+  },
+  {
+    "revision": "c61c615c4d64e0f8bd9e",
+    "url": "/monsters-rolodex/static/js/runtime-main.95c043bc.js"
+  }
+]);
